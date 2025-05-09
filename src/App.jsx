51c534958e9c652d4e56f13ea7640fe2ba653495
BegoCaZa <begoxcz@hotmail.com>
2025-05-09 17:28:18 +0200

    diseño listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import Button from './components/button/Button';
-
 const App = () => {
   return (
-    <>
-      <h1>React</h1> <Button />
-    </>
+    <div className='general-container'>
+      <input
+        type='text'
+        className='text-display'
+        placeholder='P4$5W0RD!'
+        readOnly
+      />
+      <div className='label-container'>
+        <h2 className='range-value'>LENGHT:</h2>
+        <span className='range-value'>16</span>
+      </div>
+      <div className='range-container'>
+        <span className='range-value'>4</span>
+        <input
+          type='range'
+          className='range-input'
+          min='4'
+          max='32'
+          defaultValue='16'
+          step='1'
+        />
+        <span className='range-value'>32</span>
+      </div>
+
+      <button className='button'>Generate Password</button>
+    </div>
   );
 };
 
